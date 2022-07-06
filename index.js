d8dcf2da000c62320078fbd1b2c3240afc37e9c1
O Seong <100888650+Sophiee5@users.noreply.github.com>
2022-07-06 10:37:34 +0900

    postman post-test 성공, nodemon 설치

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5005
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 분석해서 가져올 수 있게
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://seong:9900@cluster-ay.ee6ix84.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('재실행하기'))
+app.get('/', (req, res) => res.send('회원가입 완료!!! nodemon dev실행완료!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입 할떄 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
